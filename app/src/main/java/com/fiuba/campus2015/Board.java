a96f8e6714c6ab3b309aeb0b75097096c27a4108
GONZALO VELASCO <vela8624@gmail.com>
2015-03-29 13:16:25 -0300

    Creacion de pantalla de login de usuario, https://app.asana.com/0/29427082947133/29427082947146

---
+++ app/src/main/java/com/fiuba/campus2015/Board.java
@@ -0,0 +1,22 @@
+package com.fiuba.campus2015;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.internal.widget.ActionBarOverlayLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by gonzalovelasco on 29/3/15.
+ */
+public class Board extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.board_layout);
+
+    }
+}
--- app/src/main/java/com/fiuba/campus2015/MainActivity.java
+++ app/src/main/java/com/fiuba/campus2015/MainActivity.java
@@ -1,9 +1,14 @@
 package com.fiuba.campus2015;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +17,24 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user  =  ((EditText)findViewById(R.id.textuser)).getText().toString();
+                String password =  ((EditText)findViewById(R.id.textpassword)).getText().toString();
+
+                if (user.isEmpty() || password.isEmpty())
+                    Toast.makeText(getApplicationContext(),"Usuario Incorrecto",Toast.LENGTH_SHORT).show();
+                else
+                {
+                    Intent intent = new Intent(MainActivity.this, Board.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
 
     }
 
