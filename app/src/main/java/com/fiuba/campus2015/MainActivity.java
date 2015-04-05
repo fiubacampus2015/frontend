61a923425b6117b4353f11f33eb4b5cc8003d360
tapiajimena <jimena.soledad.tapia@gmail.com>
2015-04-04 21:57:11 -0300

    se establecen formatos de los inputs de pwd y celular. Se corrigen algunos colores conforme al theme elelegido.

--- app/src/main/java/com/fiuba/campus2015/MainActivity.java
+++ app/src/main/java/com/fiuba/campus2015/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,10 +21,13 @@ import retrofit.RestAdapter;
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button button = (Button) findViewById(R.id.login);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
