f18d13a66385ffaef168cda8a29ae74c7beb7c0c
karlo ismael <karlo.ismael@gmail.com>
2015-04-12 09:04:25 -0300

    me olvide agregar esta clase...

---
+++ app/src/main/java/com/fiuba/campus2015/extras/Constants.java
@@ -0,0 +1,25 @@
+package com.fiuba.campus2015.extras;
+
+/**
+ * Created by ismael on 12/04/15.
+ */
+public class Constants {
+    public static final String NAME = "name";
+    public static final String SURNAME = "surname";
+    public static final String EMAIL = "email";
+    public static final String PHONE = " phone";
+    public static final String NATIONALITY = "nationality";
+    public static final String SEX = "sex";
+    public static final String BIRTHDAY = "birthday";
+    public static final String FOTO = "foto";
+    public static final String PROFESION = "profession";
+    public static final String ORIENTATION = "orientation";
+    public static final String DATEADMISSION = "admission";
+    public static final String DESCRIPCIONEMPLEO = "descripcionE";
+    public static final String FECHAINGRESOEMPLEO = "ingreso";
+    public static final String FECHASALIDAIEMPLEO = "salida";
+    public static final String CANTIDADEMPLEOS = "totalEmpleos";
+    public static final String COMENTARIO = "comentario";
+    public static final String[] EXTENSIONES = {"png", "jpeg"};
+
+}
