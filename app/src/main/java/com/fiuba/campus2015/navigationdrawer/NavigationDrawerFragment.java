058f0dadc3cfcd7098d8b8840dfb77995ffd6feb
tapiajimena <jimena.soledad.tapia@gmail.com>
2015-04-18 19:46:14 -0300

    se cambian los iconos del menu y se agrega el link para salir de la aplicación https://app.asana.com/0/29427082947133/31827738253454

--- app/src/main/java/com/fiuba/campus2015/navigationdrawer/NavigationDrawerFragment.java
+++ app/src/main/java/com/fiuba/campus2015/navigationdrawer/NavigationDrawerFragment.java
@@ -148,8 +148,10 @@ public class NavigationDrawerFragment extends Fragment implements NavigationDraw
     /************************************/
     public List<NavigationItem> getMenu() {
         List<NavigationItem> items = new ArrayList<>();
-        items.add(new NavigationItem("Muro", getResources().getDrawable(R.drawable.ic_menu_check)));
-        items.add(new NavigationItem("Perfil", getResources().getDrawable(R.drawable.ic_menu_check)));
+        items.add(new NavigationItem("Muro", getResources().getDrawable(R.drawable.ic_dashboard_grey)));
+        items.add(new NavigationItem("Perfil", getResources().getDrawable(R.drawable.ic_account_circle_grey)));
+        items.add(new NavigationItem("Salir", getResources().getDrawable(R.drawable.ic_settings_power_grey)));
+
         return items;
     }
 
