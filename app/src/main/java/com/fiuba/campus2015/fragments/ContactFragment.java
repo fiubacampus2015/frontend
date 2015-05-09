7bada28938961e490b8bebecbc5b5a4c823261bb
GONZALO VELASCO <vela8624@gmail.com>
2015-05-09 18:08:33 -0300

    Perfil contacto completo, con tabs de muro y perfil  https://app.asana.com/0/29427082947133/33075313677366

--- app/src/main/java/com/fiuba/campus2015/fragments/ContactFragment.java
+++ app/src/main/java/com/fiuba/campus2015/fragments/ContactFragment.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.fiuba.campus2015.ProfileEditable;
+import com.fiuba.campus2015.ProfileFriend;
 import com.fiuba.campus2015.ProfileReduced;
 import com.fiuba.campus2015.R;
 import com.fiuba.campus2015.adapter.ContactItem;
@@ -95,7 +96,9 @@ public class ContactFragment extends Fragment {
                 new RecyclerItemClickListener(getActivity(), new RecyclerItemClickListener.OnItemClickListener() {
                     @Override public void onItemClick(View view, int position) {
                         User contact = contactsAdapter.getContact(position);
-                        Intent intent = new Intent(getActivity(), ProfileReduced.class);
+                        Intent intent = new Intent(getActivity(), ProfileFriend.class);
+                        //O perfil reducido
+                        //Intent intent = new Intent(getActivity(), ProfileReduced.class);
                         intent.putExtra(USER, new Gson().toJson(contact));
                         startActivity(intent);
                     }
