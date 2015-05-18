b8b5ae8f0e8b7ae8892f5f64daa97076bfa402da
karlo ismael <karlo.ismael@gmail.com>
2015-05-18 00:14:34 -0300

    borrar contacto de su lista de contactos, https://app.asana.com/0/29427082947133/34523200539818 , https://app.asana.com/0/29427082947133/34523200539998

--- app/src/main/java/com/fiuba/campus2015/adapter/ContactsAdapter.java
+++ app/src/main/java/com/fiuba/campus2015/adapter/ContactsAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
 import android.view.LayoutInflater;
@@ -14,19 +15,27 @@ import android.widget.TextView;
 
 import com.fiuba.campus2015.R;
 import com.fiuba.campus2015.dto.user.User;
+import com.fiuba.campus2015.fragments.ContactFragment;
+import com.fiuba.campus2015.services.RestClient;
+import com.gc.materialdesign.widgets.Dialog;
+import com.gc.materialdesign.widgets.ProgressDialog;
 
 import java.util.Collections;
 import java.util.List;
+
+import retrofit.client.Response;
 
 public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolderContacts> {
     private LayoutInflater layoutInflater;
     private List<User> contactsItems;
     private Context context;
     private String userId;
+    private ContactFragment contactFragment;
 
-    public ContactsAdapter(Context context){
+    public ContactsAdapter(Context context, ContactFragment contactFragment){
         layoutInflater = LayoutInflater.from(context);
         this.context = context;
+        this.contactFragment =contactFragment;
     }
 
 
@@ -60,37 +69,37 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHo
 
         holder.textViewName.setText(contactItem.name + " "+ contactItem.username);
         holder.textMail.setText(contactItem.email);
-
+        holder.adapter = this;
         holder.viewSeparator.setBackgroundColor(Color.WHITE);
 
         if(contactsItems.get(0) != contactItem) {
-                // los item que muestran la letra y son distintos al primero muestran la division
-                holder.viewSeparator.setBackgroundColor(Color.parseColor("#ffcfcfcf"));
-            }
+            // los item que muestran la letra y son distintos al primero muestran la division
+            holder.viewSeparator.setBackgroundColor(Color.parseColor("#ffcfcfcf"));
+        }
 
         //Valido si es amigo o no
-            if (contactItem.status == null || (contactItem.status!= null && contactItem.status.equals("reject")))
-            {
-                holder.viewSendInvitation.setVisibility(View.VISIBLE);
-                holder.viewDeleteContact.setVisibility(View.GONE);
-           }else {
-                holder.viewSendInvitation.setVisibility(View.GONE);
-                holder.viewDeleteContact.setVisibility(View.VISIBLE);
-            }
+        if (contactItem.status == null || (contactItem.status!= null && contactItem.status.equals("reject")))
+        {
+            holder.viewSendInvitation.setVisibility(View.VISIBLE);
+            holder.viewDeleteContact.setVisibility(View.GONE);
+        }else {
+            holder.viewSendInvitation.setVisibility(View.GONE);
+            holder.viewDeleteContact.setVisibility(View.VISIBLE);
+        }
 
-            Bitmap  icon;
-            if (contactItem.personal!= null && !contactItem.personal.photo.isEmpty())
-            {
-                byte[] decodedString = Base64.decode(contactItem.personal.photo, Base64.DEFAULT);
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inPurgeable = true;
-                icon = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length, options);
-                icon = Bitmap.createScaledBitmap(icon, 50 , 50 , true);
+        Bitmap  icon;
+        if (contactItem.personal!= null && !contactItem.personal.photo.isEmpty())
+        {
+            byte[] decodedString = Base64.decode(contactItem.personal.photo, Base64.DEFAULT);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPurgeable = true;
+            icon = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length, options);
+            icon = Bitmap.createScaledBitmap(icon, 50 , 50 , true);
 
-            }else {
-                icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_account_circle_grey_48dp);
-            }
-            holder.imageViewContact.setImageBitmap(icon);
+        }else {
+            icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_account_circle_grey_48dp);
+        }
+        holder.imageViewContact.setImageBitmap(icon);
     }
 
     @Override
@@ -102,9 +111,30 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHo
         return contactsItems.get(position);
     }
 
+    private void removeContact(final int position) {
+        final User contact = getContact(position);
+        Dialog dialog = new Dialog(contactFragment.getActivity(),null, "Esta seguro que desea eliminar su contacto");
+        dialog.setOnAcceptButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contactsItems.remove(position);
+                notifyItemRemoved(position);
+                contactFragment.removeContact(contact);
+            }
+        });
+        dialog.addCancelButton("Cancelar");
+        dialog.show();
+        dialog.getButtonAccept().setText("Aceptar");
 
-    static class ViewHolderContacts extends RecyclerView.ViewHolder {
 
+    }
+
+    private void setUserSelected(int position) {
+        contactFragment.loadProfile(getContact(position));
+    }
+
+    static class ViewHolderContacts extends RecyclerView.ViewHolder  implements View.OnClickListener {
+        ContactsAdapter adapter;
         TextView textViewName;
         ImageView imageViewContact;
         View viewSeparator;
@@ -120,7 +150,21 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHo
             viewSendInvitation = (ImageView) itemView.findViewById(R.id.sendInvitation);
             viewDeleteContact = (ImageView) itemView.findViewById(R.id.deleteContact);
             textMail = (TextView)itemView.findViewById(R.id.mailText);
+            itemView.setOnClickListener(this);
+            viewDeleteContact.setOnClickListener(this);
+        }
 
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.deleteContact:
+                    adapter.removeContact(getAdapterPosition());
+                    break;
+                default:
+                    adapter.setUserSelected(getAdapterPosition());
+            }
         }
     }
+
+
 }
